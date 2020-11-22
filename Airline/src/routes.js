329925adb55dfbc1d8cde743c25fd3bbd3d76117
WebMaster <super.web.dev@yandex.com>
2020-11-21 21:37:03 -0800

    Revert "Revert "flights page completion 1.1""
    This reverts commit 4008a687bfb259e5dec527439dd220b5771832f7.

--- Airline/src/routes.js
+++ Airline/src/routes.js
@@ -27,6 +27,7 @@ import Language from "@material-ui/icons/Language";
 // core components/views for Admin layout
 import DashboardPage from "views/Dashboard/Dashboard.js";
 import UserProfile from "views/UserProfile/UserProfile.js";
+import Flights from "views/UserProfile/Flights.js";
 import TableList from "views/TableList/TableList.js";
 import Typography from "views/Typography/Typography.js";
 import Icons from "views/Icons/Icons.js";
@@ -35,76 +36,42 @@ import NotificationsPage from "views/Notifications/Notifications.js";
 import UpgradeToPro from "views/UpgradeToPro/UpgradeToPro.js";
 // core components/views for RTL layout
 import RTLPage from "views/RTLPage/RTLPage.js";
+import { AirlineSeatFlat, AirplanemodeActive } from "@material-ui/icons";
 
 const dashboardRoutes = [
   {
     path: "/dashboard",
-    name: "Dashboard",
-    rtlName: "لوحة القيادة",
+    name: "Dashboard / Admin",
     icon: Dashboard,
     component: DashboardPage,
     layout: "/admin"
   },
   {
     path: "/user",
-    name: "User Profile",
-    rtlName: "ملف تعريفي للمستخدم",
+    name: "Check In / Stuff",
     icon: Person,
-    component: UserProfile,
+    component: Flights,
     layout: "/admin"
   },
   {
     path: "/table",
-    name: "Table List",
-    rtlName: "قائمة الجدول",
-    icon: "content_paste",
+    name: "In Flight / Stuff",
+    icon: AirplanemodeActive,
     component: TableList,
     layout: "/admin"
   },
+
   {
     path: "/typography",
     name: "Typography",
-    rtlName: "طباعة",
     icon: LibraryBooks,
     component: Typography,
     layout: "/admin"
   },
-  {
-    path: "/icons",
-    name: "Icons",
-    rtlName: "الرموز",
-    icon: BubbleChart,
-    component: Icons,
-    layout: "/admin"
-  },
-  {
-    path: "/maps",
-    name: "Maps",
-    rtlName: "خرائط",
-    icon: LocationOn,
-    component: Maps,
-    layout: "/admin"
-  },
-  {
-    path: "/notifications",
-    name: "Notifications",
-    rtlName: "إخطارات",
-    icon: Notifications,
-    component: NotificationsPage,
-    layout: "/admin"
-  },
-  {
-    path: "/rtl-page",
-    name: "RTL Support",
-    rtlName: "پشتیبانی از راست به چپ",
-    icon: Language,
-    component: RTLPage,
-    layout: "/rtl"
-  },
+
   {
     path: "/upgrade-to-pro",
-    name: "Upgrade To PRO",
-    rtlName: "التطور للاحترافية",
+    name: "To Airline Pages",
     icon: Unarchive,
     component: UpgradeToPro,
     layout: "/admin"
